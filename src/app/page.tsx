f95fb745aedb3b0e3c8414ae6f767266cceba3c2
anirudhUN <anirudha.un@thoughtclan.com>
2025-07-03 15:12:20 +0530

    updated type error in handlesubmit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function LoginPage() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
       const response = await axios.post(
